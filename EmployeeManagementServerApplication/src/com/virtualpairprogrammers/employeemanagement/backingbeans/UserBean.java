54b1a464b68f792c0578569ade0585f6b602512c
elBibi <my.ajt.knight@gmail.com>
2021-10-27 11:49:54 -0400

    added page welcome.xhml,hello.xhmtl, BackingBean/UserBean.java

---
+++ EmployeeManagementServerApplication/src/com/virtualpairprogrammers/employeemanagement/backingbeans/UserBean.java
@@ -0,0 +1,18 @@
+package com.virtualpairprogrammers.employeemanagement.backingbeans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="user")
+public class UserBean {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
